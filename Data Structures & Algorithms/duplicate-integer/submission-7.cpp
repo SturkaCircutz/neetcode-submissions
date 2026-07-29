13d12b9bb3e4a623f42e65290e5a4e2c96fb93fb
sturka <313721325sjw@gmail.com>
2026-07-29 09:39:09 +0800

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> mp;
+        for(auto n:nums){
+            if(mp.count(n)){
+                return true;
+            }
+            mp.insert(n);
+    
+        }
+        return false;
+    }
+
+};
