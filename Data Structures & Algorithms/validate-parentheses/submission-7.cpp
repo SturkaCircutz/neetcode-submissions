63c5366ab55d15e394d65a96a02985eaaf41ad6a
sturka <313721325sjw@gmail.com>
2026-08-03 12:02:05 +0800

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> mp = {
+            {']', '['},
+            {'}', '{'},
+            {')', '('},
+        };
+        stack<char> sta;
+        for(auto c : s){
+            if(!sta.empty() && mp.count(c)){
+                if(sta.top() != mp[c]){
+                    return false;
+                }
+                sta.pop();
+            }
+            else{
+                sta.push(c);
+            }
+        }
+        return sta.empty();
+    }
+};
