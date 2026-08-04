da0c47eb0be80035990c1a93943cde6cd5f2443e
sturka <313721325sjw@gmail.com>
2026-08-04 13:26:04 +0800

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 1;
+        int res = 0;
+        while( r< prices.size()){
+            if(prices[r] > prices[l]){
+                res = max(res, prices[r]-prices[l]);
+            }else{
+                l = r;
+            }
+            r++;
+        }
+        return res;
+    }
+};
