44d2cd9ca41d6fe286f074f9779ba335822f3241
sturka <313721325sjw@gmail.com>
2026-08-09 10:14:55 +0800

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l = 0, r= 0;
+        int far = 0;
+        int res = 0;
+        while(r < nums.size()-1){
+            for(int i = l; i <= r; i++){
+                 far = max(far, nums[i]+i);
+            }
+            l = r+1;
+            r = far;
+            res ++;
+        }
+        return res;
+    }
+};
